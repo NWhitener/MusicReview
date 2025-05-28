8570a1ab75f8aeef97bc03f97a9850508c06c92a
NWhitener <nathanpwhitener@gmail.com>
2025-05-27 20:36:54 -0400

    Table creation script for all the tables in the database

--- db/table_creation.py
+++ db/table_creation.py
@@ -1,4 +1,56 @@
-from sqlalchemy import create_engine, Integer, String, Float, ForeignKey  
+from sqlalchemy import create_engine, Integer, String, Float, ForeignKey, MetaData, Column, DateTime, Table
+
+engine = create_engine('sqlite+pysqlite:///musicreview.db')
 
 
+meta = MetaData()
 
+users = Table(
+    'users', 
+    meta, 
+    Column('user_id', Integer, primary_key = True), 
+    Column('username', String, nullable = False), 
+    Column('passwd_hash', String, nullable = False), 
+    Column('created_at', DateTime, nullable = False)
+)
+
+song = Table(
+    'song', 
+    meta, 
+    Column('song_id', Integer, primary_key = True), 
+    Column('song_name', String, nullable = False), 
+    Column('artist_id', Integer, ForeignKey('artist.artist_id')), 
+    Column('album_id', Integer, ForeignKey('album.album_id')),
+    Column('run_time', Float, nullable = True), 
+    Column('genre', String, nullable = True)
+)
+
+artist = Table(
+    'artist', 
+    meta, 
+    Column('artist_id', Integer, primary_key = True), 
+    Column('artist_name', String, nullable = False), 
+    Column('artist_genre', String, nullable = True), 
+    Column('artist_albums', String, nullable = True)
+)
+
+reviews = Table(
+    'reviews', 
+    meta, 
+    Column('review_id', Integer, primary_key = True), 
+    Column('review_user', Integer, ForeignKey('users.user_id')), 
+    Column('review_song', Integer, ForeignKey('song.song_id')), 
+    Column('review_rating', Float, nullable = True), 
+    Column('review_text', String, nullable = True)
+)
+
+album = Table(
+    'album', 
+    meta, 
+    Column('album_id', Integer, primary_key = True), 
+    Column('album_name', String, nullable = False), 
+    Column('release_year', String, nullable = False), 
+    Column('artist_id', Integer, ForeignKey('artist.artist_id'))
+)
+
+meta.create_all(engine)
